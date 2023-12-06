28c89c64b6f94d8c47fbceca65e46c575f84eed1
William Parker <william.parker@infineon.com>
2023-12-06 09:50:51 +0100

    Added stub for games api

---
+++ server/app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_restplus import Resource, Api, Namespace
+from flask_cors import CORS
+from .util import get_config
+
+games_api = Namespace("games", description="Creating and playing games")
+
+
+@games_api.rout("")
+class Games(Resource):
+    pass
+
+
+class OneGame(Resource):
+    pass
+
+
+# Create the app and configure it
+app = Flask(__name__)
+app.config.update(get_config(app.config["ENV"]), app.open_resource("config.yaml"))
+CORS(app)
+api = Api(app)
+api.add_namespace(games_api, path="/api/games")
