52e171c5b0847cf154fc740fb41713782a79f278
William Parker <william.parker@infineon.com>
2024-01-05 08:56:16 +0100

    Debugging

--- server/app.py
+++ server/app.py
@@ -17,6 +17,10 @@ class OneGame(Resource):
 
 # Create the app and configure it
 app = Flask(__name__)
+print(list(app.config))
+# TODO: This Fails because ENV is no-longer a configuration used internally by FLASK.  Check to see the docs for the
+# corresponding FLASK version used in the tutorial. https://flask.palletsprojects.com/en/3.0.x/config/#builtin-configuration-values
+tmp = app.config["ENV"]
 app.config.update(get_config(app.config["ENV"], app.open_resource("config.yaml")))
 CORS(app)
 api = Api(app)
