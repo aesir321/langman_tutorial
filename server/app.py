af6685febfc5aafd21a5ab67440456f79ebcd8f8
William Parker <william.parker@infineon.com>
2024-01-15 08:45:41 +0100

    Added method to guess letters

--- server/app.py
+++ server/app.py
@@ -83,6 +83,70 @@ class Games(Resource):
 
 @games_api.route("/<game_id>")
 class OneGame(Resource):
+    def _check_game_exists(self, game_id):
+        """Checks whether game ``game_id`` exists, if yes returns Game ``game_id``, otherwise aborts request.
+
+        Args:
+            game_id (str): uuid assigned to game that needs to be checked.
+
+        Returns:
+            _type_: _description_
+        """
+        # check input is valid
+        game = g.games_db.query(Game).filter(Game.game_id == game_id).one_or_none()
+
+        # if game does not exist, produce error code
+        if game is None:
+            games_api.abort(404, f"Game with id {game_id} does not exist.")
+
+        return game
+
+    def _get_letter(self):
+        """checks if a valid letter is present in the payload if it is the letter is returned, otherwise the request is aborted.
+
+        Returns:
+            _type_: _description_
+        """
+        if (
+            "letter" not in games_api.payload
+            or not games_api.payload["letter"].isalpha()
+            or len(games_api.payload["letter"]) != 1
+        ):
+            games_api.abort(
+                400, 'PUT requires one alphabetic character in the "letter" field.'
+            )
+        letter = games_api.payload["letter"].lower()
+
+        return letter
+
+    def _update_game_with_guess(self, letter, game, usage):
+        """Updates a game with the corresponding guessed letter
+
+        Args:
+            letter (str): The guessed letter
+            game (Game): The Game currently being played
+            usage (Usage):
+        """
+        if letter in game.guessed:
+            games_api.abort(403, f"Letter {letter} was already guessed.")
+        game.guessed = game.guessed + letter
+        if letter in usage.secret_word.lower():
+            game.reveal_word = "".join(
+                [
+                    guessed_letter if guessed_letter.lower() in game.guessed else "_"
+                    for guessed_letter in usage.secret_word
+                ]
+            )
+        else:
+            game.bad_guesses += 1
+
+        return game
+
+    def _update_user(self, game):
+        user = g.games_db.query(User).filter(User.user_id == game.player).one()
+        game.end_time = datetime.datetime.now()
+        user._game_ended(game._result(), game.end_time - game.start_time)
+
     def get(self, game_id):
         """
         Get the game ``game_id`` information.
@@ -104,12 +168,8 @@ class OneGame(Resource):
                 * ``lang`` The language of the example such as "en"
                 * ``source`` The book from which the usage example originated
         """
-        # check input is valid
-        game = g.games_db.query(Game).filter(Game.game_id == game_id).one_or_none()
+        game = self._check_game_exists(game_id=game_id)
 
-        # if game does not exist, produce error code
-        if game is None:
-            games_api.abort(404, f"Game with id {game_id} does not exist.")
         # get usage record because it contains the language and usage example
         usage = g.usage_db.query(Usage).filter(Usage.usage_id == game.usage_id).one()
 
@@ -122,9 +182,54 @@ class OneGame(Resource):
 
     def put(self, game_id):
         """
-        Guess a letter and update the game state accordingly.
+        Update game ``game_id`` as resulting from a guessed letter.
+
+        :route: ``/<game_id>`` PUT
+
+        :payload:
+            The guessed letter as an object:
+                * ``letter`` A single guessed letter
+
+        :returns:
+            The object for a game, including:
+                * ``game_id`` The game's UUID
+                * ``player`` The player's name
+                * ``usage_id`` The game usage id from the Usages table
+                * ``guessed`` A string of guessed letters
+                * ``reveal_word`` Guessed letters in otherwise blanked word string
+                * ``bad_guesses`` Number of incorrect guesses so far
+                * ``start_time`` The epoch ordinal time when game began
+                * ``end_time`` The epoch ordinal time when game ended
+                * ``result`` Game outcome from ("lost", "won", "active")
+
+            This method interacts with the database to update the indicated game
         """
-        return {"message": "Game PUT under construction"}
+        # check input is valid; return error if game non-existent or inactive
+        game = self._check_game_exists(game_id=game_id)
+
+        if game._result() != "active":
+            games_api.abort(403, f"Game with id {game_id} is over.")
+
+        letter = self._get_letter()
+        usage = g.usage_db.query(Usage).filter(Usage.usage_id == game.usage_id).one()
+        self._update_game_with_guess(letter, game, usage)
+
+        # if game is over, update the user record
+        outcome = game._result()
+        if outcome != "active":
+            self._update_user(game)
+
+        # return the modified game state
+        game_dict = game._to_dict()
+        game_dict["usage"] = usage.usage.format(word="_" * len(usage.secret_word))
+        game_dict["lang"] = usage.language
+        game_dict["source"] = usage.source
+
+        if outcome != "active":
+            game_dict["secret_word"] = usage.secret_word
+
+        g.games_db.commit()
+        return game_dict
 
     def delete(self, game_id):
         """Delete record for game ``game_id``
@@ -138,13 +243,11 @@ class OneGame(Resource):
 
         This method removed the game from its table
         """
-        game = g.games_db.query(Game).filter(Game.game_id == game_id).one_or_none()
-        if game is not None:
-            g.games_db.delete(game)
-            g.games_db.commit()
-            message = "One record deleted"
-        else:
-            message = "Zero records deleted"
+        game = self._check_game_exists(game_id=game_id)
+        g.games_db.delete(game)
+        g.games_db.commit()
+        message = "One record deleted"
+
         return {"message": message}
 
 
